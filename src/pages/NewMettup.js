b1241a7a7f0de2cde6b793910c728d1d8b1453e7
k1999-darouich <k.darouich_etu@enset-media.ac.ma>
2021-07-16 17:48:55 +0100

    AddContext

--- src/pages/NewMettup.js
+++ src/pages/NewMettup.js
@@ -1,7 +1,10 @@
+import { useHistory } from 'react-router-dom';
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+    const history = useHistory();
     function addMeetupHandler(meetupData) {
+
         fetch(
             'https://meetup-ebda0-default-rtdb.firebaseio.com/meetups.json'
             , {
@@ -11,7 +14,9 @@ function NewMeetupPage() {
                     'Content-Type': 'application/json'
                 }
             }
-        );
+        ).then(() => {
+            history.replace('/');
+        });
     }
 
     return (<div>
